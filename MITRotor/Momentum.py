0b64869a822300f026554f49e29b28f5cdb100bc
iupfal <iupfal@mit.edu>
2025-02-13 09:35:09 -0500

    add corrections and averaging schemes to classical momentum model

--- MITRotor/Momentum.py
+++ MITRotor/Momentum.py
@@ -58,8 +58,21 @@ class ConstantInduction(MomentumModel):
 
 
 class ClassicalMomentum(MomentumModel):
-    def Ct_a(self, Ct, yaw, tiploss=1.0):
-        return 0.5 * (1 - np.sqrt(1 - Ct / tiploss))
+    def __init__(
+            self, averaging: Literal["sector", "annulus", "rotor"] = "rotor"
+    ):
+        if averaging == "rotor":
+            self._func = self._func_rotor
+        elif averaging == "annulus":
+            self._func = self._func_annulus
+        elif averaging == "sector":
+            self._func = self._func_sector
+        else:
+            raise ValueError(f"Averaging method {averaging} not found for ClassicalMomentum model.")
+        self.averaging = averaging
+
+    def Ct_a(self, Ct, yaw):
+        return 0.5 * (1 - np.sqrt(1 - Ct))
 
     def __call__(
         self,
@@ -70,10 +83,64 @@ class ClassicalMomentum(MomentumModel):
         rotor: "RotorDefinition",
         geom: "BEMGeometry",
     ) -> ArrayLike:
-        Ct = aero_props.solidity * geom.annulus_average(aero_props.W**2 * aero_props.Cax)
-        a = self.Ct_a(Ct, yaw, tiploss=aero_props.F)
+        an = self._func(aero_props, pitch, tsr, yaw, rotor, geom)
+        return an
+    
+    def _func_rotor(
+        self,
+        aero_props: "AerodynamicProperties",
+        pitch: float,
+        tsr: float,
+        yaw: float,
+        rotor: "RotorDefinition",
+        geom: "BEMGeometry",
+    ) -> ArrayLike:
+
+        # Compute the thrust coefficient
+        Ct = aero_props.solidity * aero_props.W**2 * aero_props.Cax
+        # Average the thrust coefficient over the rotor
+        Ct_bar = geom.rotor_average(geom.annulus_average(Ct))
+        # Compute the average axial induction factor
+        a_bar = self.Ct_a(Ct_bar, yaw)
+        # Compute the average tip loss factor
+        f_bar = geom.rotor_average(geom.annulus_average(aero_props.F))
+        # Compute the corrected average axial induction factor
+        a_corr = np.ones_like(geom.mu_mesh) * (a_bar / f_bar)
+
+        return a_corr
+    
+    def _func_annulus(
+        self,
+        aero_props: "AerodynamicProperties",
+        pitch: float,
+        tsr: float,
+        yaw: float,
+        rotor: "RotorDefinition",
+        geom: "BEMGeometry",
+    ) -> ArrayLike:
+        
+        # Compute the annulus averaged thrust coefficient
+        Ct = geom.annulus_average(aero_props.solidity * aero_props.W**2 * aero_props.Cax)
+        # Compute the annulus averaged axial induction factor and apply the tip loss correction
+        a = self.Ct_a(Ct, yaw)[:, None] * np.ones(geom.shape) / aero_props.F
 
         return a
+    
+    def _func_sector(
+        self,
+        aero_props: "AerodynamicProperties",
+        pitch: float,
+        tsr: float,
+        yaw: float,
+        rotor: "RotorDefinition",
+        geom: "BEMGeometry",
+    ) -> ArrayLike:
+
+        # Compute the thrust coefficient
+        Ct = aero_props.solidity * aero_props.W**2 * aero_props.Cax
+        # Compute the axial induction factor and apply the tip loss correction
+        a = self.Ct_a(Ct.ravel(), yaw)
+        return a.reshape(geom.shape) / aero_props.F
 
 
 class HeckMomentum(MomentumModel):
