7de95b3f9b018d6c0ca1a1109a4c66700af08960
Ilan Upfal <103550915+iupfal@users.noreply.github.com>
2025-02-26 21:44:27 -0500

    edits __init__

--- MITRotor/__init__.py
+++ MITRotor/__init__.py
@@ -1,7 +1,7 @@
 from .Geometry import BEMGeometry
-from .Aerodynamics import AerodynamicProperties, AerodynamicModel, DefaultAerodynamics, KraghAerodynamics
+from .Aerodynamics import AerodynamicProperties, AerodynamicModel, DefaultAerodynamics
 from .TangentialInduction import TangentialInductionModel, DefaultTangentialInduction, NoTangentialInduction
 from .TipLoss import TipLossModel, NoTipLoss, PrandtlTipLoss
 from .Momentum import MomentumModel, ConstantInduction, ClassicalMomentum, HeckMomentum, UnifiedMomentum, MadsenMomentum
 from .BEMSolver import BEM, BEMSolution
-from .ReferenceTurbines import IEA15MW, IEA10MW, IEA3_4MW
+from .ReferenceTurbines import IEA15MW, IEA10MW, IEA3_4MW, NREL5MW, IEA22MW
