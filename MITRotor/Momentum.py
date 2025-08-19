c02a1bc9652a420c93b3a34de1f42f0ba13abdd3
Ilan Upfal <103550915+iupfal@users.noreply.github.com>
2025-08-19 13:10:56 -0400

    edit momentum call

--- MITRotor/Momentum.py
+++ MITRotor/Momentum.py
@@ -56,7 +56,7 @@ class MomentumModel(ABC):
         rotor_avg_axial_force = (
             geom.rotor_average(
                 geom.annulus_average(
-                    np.clip(aero_props.C_x_corr, 0, 1.69)
+                    np.clip(aero_props.C_x_corr, 0, 3)
                     ) *
                     (geom.mu > rotor.hub_radius / rotor.R)  # Only consider points outside the hub
                     )
@@ -79,7 +79,8 @@ class MomentumModel(ABC):
         annulus_avg_axial_force = (
             
                 geom.annulus_average(
-                    np.clip(aero_props.C_x_corr, 0, 1.69)
+                    np.clip(aero_props.C_x_corr, 0, 3) *
+                    (geom.mu > rotor.hub_radius / rotor.R)  # Only consider points outside the hub
                     )
                     )[:, None] * np.ones(geom.shape)
         
