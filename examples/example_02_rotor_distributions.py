2d8396e23bd0fd6ae032351e95cf0526bc8a0dcf
Ilan Upfal <103550915+iupfal@users.noreply.github.com>
2026-02-06 11:41:47 -0500

    clarify example 2

--- examples/example_02_rotor_distributions.py
+++ examples/example_02_rotor_distributions.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 
-from MITRotor import BEM, BEMGeometry, IEA10MW, BEMSolution
+from MITRotor import BEM, BEMGeometry, IEA10MW, BEMSolution, MadsenMomentum
 
 figdir = Path("fig")
 figdir.mkdir(exist_ok=True, parents=True)
@@ -11,13 +11,15 @@ figdir.mkdir(exist_ok=True, parents=True)
 
 def plot_radial_distributions(sol: BEMSolution, save_to: Path):
     fig, axes = plt.subplots(3, 1, sharex=True)
-    axes[0].plot(sol.geom.mu, sol.Cp(grid="annulus"), label="$C_P$")
-    axes[1].plot(sol.geom.mu, sol.Ct(grid="annulus"), label="$C_T$")
-    axes[2].plot(sol.geom.mu, sol.a(grid="annulus"), label="$a_n$")
+    axes[0].plot(sol.geom.mu, sol.Cp(grid="annulus"))
+    axes[1].plot(sol.geom.mu, sol.Ct(grid="annulus"))
+    axes[2].plot(sol.geom.mu, sol.a(grid="annulus"))
 
-    [ax.legend(loc="lower center") for ax in axes]
 
-    axes[-1].set_xlabel("Radial position, $\mu$ [-]")
+    axes[0].set_ylabel("$C_P$")
+    axes[1].set_ylabel("$C_T$")
+    axes[2].set_ylabel("$a_n$")
+    axes[-1].set_xlabel("$\mu$")
     plt.xlim(0, 1)
     plt.savefig(save_to, dpi=300, bbox_inches="tight")
 
@@ -45,7 +47,7 @@ def plot_azimuthal_variations(sol: BEMSolution, save_to: Path):
 if __name__ == "__main__":
     # Initialize rotor with increased radial resolution.
     rotor = IEA10MW()
-    bem = BEM(rotor=rotor, geometry=BEMGeometry(Nr=100, Ntheta=10))
+    bem = BEM(rotor=rotor, geometry=BEMGeometry(Nr=100, Ntheta=10), momentum_model=MadsenMomentum(averaging='annulus'))
 
     # solve BEM for a control set point.
     pitch, tsr, yaw = np.deg2rad(0), 7.0, np.deg2rad(0.0)
